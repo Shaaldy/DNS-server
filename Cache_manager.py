55df986bb3296c71a012c903e3d9212d2fdadc5f
Shaaldy <shaldeeman@gmail.com>
2024-07-14 20:39:27 +0500

    логика

--- Cache_manager.py
+++ Cache_manager.py
@@ -21,16 +21,30 @@ class CacheManager:
 
     def save_cache_to_disk(self):
         with open(self.CACHE_FILE, 'wb') as file:
+            pickle.dump(0, file)
+            file.close()
+        with open(self.CACHE_FILE, 'wb') as file:
             pickle.dump(self.cache, file)
+            file.close()
+            print("Cache saved to disk")
 
     def load_cache_from_disk(self):
         if os.path.exists(self.CACHE_FILE):
             with open(self.CACHE_FILE, 'rb') as file:
                 self.cache = pickle.load(file)
+                file.close()
                 print("Loaded cache from disk")
 
     def remove_expired_cache(self):
         cur_time = time.time()
-        for key, (value, ttl) in self.cache.items():
-            if ttl < cur_time:
-                del self.cache[key]
+        if len(self.cache.items()) == 0:
+            return
+        to_del = []
+        for key, records in self.cache.items():
+            for record, (value, ttl) in records.items():
+                if ttl < cur_time:
+                    to_del.append((key, record))
+        for key, to_del_item in to_del:
+            self.cache[key].pop(to_del_item)
+            print(f"Expired cache {to_del_item} has been removed")
+
--- DNSServer.py
+++ DNSServer.py
@@ -19,10 +19,11 @@ class DnsServer:
             print(f"Server started on {self.ip}:{self.port}.")
             dns_res = DNSResolver()
             while True:
+                dns_res.check_actual_data()
                 data, addr = s.recvfrom(1024)
-                print(f"Get response from {addr}")
                 dns_request = DNSRecord.parse(data)
                 dns_response = dns_res.dns_resolve(dns_request)
+                print(f"Get response from {addr}")
                 s.sendto(dns_response.pack(), addr)
                 print(f"Sending response to {addr}")
 
@@ -33,7 +34,6 @@ class DNSResolver:
         self.cache_manager.load_cache_from_disk()
 
     def dns_resolve(self, dns_request):
-        print(self.cache_manager.cache)
         qtype = dns_request.q.qtype
         key = dns_request.q.qname
 
@@ -55,6 +55,10 @@ class DNSResolver:
         self.decrease_ttl(cache)
         return response_record
 
+    def check_actual_data(self):
+        self.cache_manager.remove_expired_cache()
+        self.cache_manager.save_cache_to_disk()
+
     @staticmethod
     def decrease_ttl(cache):
         current_time = time.time()
@@ -64,15 +68,55 @@ class DNSResolver:
 
     @staticmethod
     def send_request_to_server(dns_request):
-        try:
+        def query_server(server, dns_request):
             with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
                 s.settimeout(15)
-                upstream_server = ("8.8.8.8", 53)
-                s.sendto(dns_request.pack(), upstream_server)
-                data, _ = s.recvfrom(1024)
-                return DNSRecord.parse(data)
-        except socket.timeout:
-            print("Timed out while sending request to upstream server")
+                try:
+                    s.sendto(dns_request.pack(), server)
+                    data, _ = s.recvfrom(1024)
+                    return DNSRecord.parse(data)
+                except socket.timeout:
+                    print(f"Timeout while querying server {server}")
+                    return None
+
+        upstream_servers = [
+            ("192.5.5.241", 53),  # f.root-servers.net
+            ("192.203.230.10", 53),  # e.root-servers.net
+            ("192.58.128.30", 53)  # j.root-servers.net
+        ]
+
+        for server in upstream_servers:
+            response = query_server(server, dns_request)
+            if response:
+                break
+
+        if not response:
+            print("Timed out while querying all upstream servers")
             return None
 
+        while response.header.a == 0:
+            authority_section = response.auth
+            additional_section = response.ar
 
+            if not authority_section:
+                print("No authority section found in the response")
+                return None
+
+            ns_record = authority_section[0]
+            ns_name = str(ns_record.rdata)
+
+            next_server = None
+            for record in additional_section:
+                rname = str(record.rname)
+                if rname == ns_name and record.rtype == QTYPE.A:
+                    next_server = str(record.rdata)
+                    break
+
+            if not next_server:
+                break
+
+            response = query_server((next_server, 53), dns_request)
+            if not response:
+                break
+
+        return response
